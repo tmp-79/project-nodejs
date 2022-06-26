8cd22dc305dc802d6981fba54e8d471843e150c5
tmp-79 <asagosv2@gmail.com>
2022-06-26 16:34:00 +0700

    hehhe

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const router = require('./routers/index');
 const app = express();
-const swaggerJsDoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
-const newLocal = 'dotenv';
 require('dotenv').config()
 
 //connect DB
@@ -15,24 +11,14 @@ connectDb();
 
 app.use(express.json());
 
-const swaggerOptions = {
-    swaggerDefinition: {
-        info: {
-            title: 'Categories REST API',
-            description: "A REST API built with Express and MongoDB. This API provides movie categories and the context of the catchphrase in the movie."
-        },
-    },
-    apis: ["./routers/category/index.js"]
-}
 
 const corsOptions = "http://localhost:4200";
 // app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use(cors(corsOptions));
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-app.use('/api',rootRouter);
+app.use('/api', rootRouter);
 
-const server = app.listen(process.env.PORT || 5000, () => {
-    const port = server.address().port;
+app.listen(process.env.PORT || 5000, () => {
     console.log(`Up and running 🚀🚀🚀🚀🚀🚀 ${port} 🚀🚀🚀🚀🚀🚀`)
 });
